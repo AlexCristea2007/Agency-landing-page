4cc10f7a0719c554de299b2b41a81ab9c65765dd
AlexCristea2007 <145534899+AlexCristea2007@users.noreply.github.com>
2024-06-18 15:48:49 +0300

    Add files via upload

---
+++ sunnyside-agency-landing-page-main/script.js
@@ -0,0 +1,11 @@
+function showMobileMenu() {
+    let menu = 
+    document.querySelector(".dropdown-mobile-menu");
+    
+    if (menu.style.display = "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
+
